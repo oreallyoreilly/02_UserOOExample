cd6fb5d8d508f65dbcc5730cf3ab4fd6751804d1
OReallyOReilly <oreallyoreillydev@gmail.com>
2018-01-07 21:35:59 +0000

    initial commit

---
+++ src/main/java/com/oreallyoreilly/UserOOExample/App.java
@@ -0,0 +1,13 @@
+package com.oreallyoreilly.UserOOExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
